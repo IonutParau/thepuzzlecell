06eadebecbfe17a51293c6dbb5ee72b826779132
IonutParau <ionut.alex.parau@gmail.com>
2022-05-04 21:03:55 +0200

    Fixed a bug

--- lib/logic/lang.dart
+++ lib/logic/lang.dart
@@ -37,7 +37,7 @@ List<File> get langs {
     langDir.createSync();
   }
 
-  return (langDir.listSync()..removeWhere((f) => f.path.endsWith(".json")))
+  return (langDir.listSync()..removeWhere((f) => !f.path.endsWith(".json")))
       .map<File>((item) => item as File)
       .toList()
     ..addAll(externalLangs);
