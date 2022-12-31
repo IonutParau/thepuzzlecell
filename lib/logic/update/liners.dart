1a9396c6fddef47dba259c7c59ecb4d3eac5ce40
uku1928 <86157399+uku1928@users.noreply.github.com>
2022-12-31 13:16:17 +0200

    fix liners

--- lib/logic/update/liners.dart
+++ lib/logic/update/liners.dart
@@ -8,7 +8,7 @@ void liners() {
       (cell, x, y) {
         if (cell.rot != rot) return;
         if (push(x, y, cell.rot, 0)) {
-          pull(frontX(x, cell.rot + 2), frontY(y, cell.rot + 2), dir, 1);
+          pull(frontX(x, cell.rot + 2), frontY(y, cell.rot + 2), cell.rot, 1);
         }
       },
       rot,
