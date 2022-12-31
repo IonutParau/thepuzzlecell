c88621eb67c0baead614fbb5615b8be0ec1dbd22
uku1928 <86157399+uku1928@users.noreply.github.com>
2022-12-31 12:47:46 +0200

    axis pulling cleaned up

--- lib/logic/update/axis.dart
+++ lib/logic/update/axis.dart
@@ -2,9 +2,7 @@ part of logic;
 
 void doAxis(int x, int y, int dir) {
   if (push(x, y, dir, 0)) {
-    final bx = x + (dir % 2 == 0 ? dir - 1 : 0);
-    final by = y + (dir % 2 == 1 ? dir - 2 : 0);
-    pull(bx, by, dir, 1);
+    pull(frontX(x, cell.rot + 2), frontY(y, cell.rot + 2), dir, 1);
     grabSide(x, y, dir - 1, dir);
     grabSide(x, y, dir + 1, dir);
   }
