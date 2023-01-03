bb5bc6b7020a0dc36b912623ec9162f45a82533b
uku1928 <86157399+uku1928@users.noreply.github.com>
2023-01-03 15:39:14 +0200

    clean driller

--- lib/logic/update/driller.dart
+++ lib/logic/update/driller.dart
@@ -15,21 +15,11 @@ void drillers() {
 
 bool doDriller(int x, int y, int dir) {
   if (!canMove(x, y, dir, 0, MoveType.mirror)) return false;
-  if (!nudge(x, y, dir, mt: MoveType.mirror)) {
-    final fx = frontX(x, dir);
-    final fy = frontY(y, dir);
-    final f = safeAt(fx, fy);
-    if (f != null) {
-      if (canMove(fx, fy, (dir + 2) % 4, 1, MoveType.mirror)) {
-        swapCells(x, y, frontX(x, dir), frontY(y, dir));
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
-    }
-  } else {
-    return true;
-  }
+  if (nudge(x, y, dir, mt: MoveType.mirror)) return true;
+  final fx = frontX(x, dir);
+  final fy = frontY(y, dir);
+  if (safeAt(fx, fy) == null) return false;
+  if (!canMove(fx, fy, (dir + 2) % 4, 1, MoveType.mirror)) return false;
+  swapCells(x, y, frontX(x, dir), frontY(y, dir));
+  return true;
 }
