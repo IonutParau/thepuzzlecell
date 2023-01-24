aba0d451b38168dc6802b267726e8700fcc28b03
IonutParau <ionut.alex.parau@gmail.com>
2023-01-24 12:24:25 +0100

    Finished new instruction set

--- lib/logic/update/code.dart
+++ lib/logic/update/code.dart
@@ -10,6 +10,33 @@ class CodeCellInstruction {
 class CodeJump {
   int ip;
   CodeJump(this.ip);
+
+  int get hashCode => ip;
+
+  bool operator ==(Object other) {
+    if (other is! CodeJump) return false;
+    return other.ip == ip;
+  }
+
+  bool operator >(Object other) {
+    if (other is! CodeJump) return false;
+    return other.ip > ip;
+  }
+
+  bool operator >=(Object other) {
+    if (other is! CodeJump) return false;
+    return other.ip >= ip;
+  }
+
+  bool operator <=(Object other) {
+    if (other is! CodeJump) return false;
+    return other.ip <= ip;
+  }
+
+  bool operator <(Object other) {
+    if (other is! CodeJump) return false;
+    return other.ip < ip;
+  }
 }
 
 class CodeStack {
@@ -94,8 +121,16 @@ class CodeStack {
     push(get(i).toString());
   }
 
+  void pushJump(int ip) {
+    push(CodeJump(ip));
+  }
+
   void push(dynamic val) {
     stack.add(val);
+  }
+
+  void pushFrom(int i) {
+    push(get(i));
   }
 
   void swap(int i, int j) {
@@ -112,12 +147,12 @@ class CodeStack {
     push(get(i));
   }
 
-  void isNum(int i) => get(i) is num;
-  void isStr(int i) => get(i) is String;
-  void isBool(int i) => get(i) is bool;
-  void isJump(int i) => get(i) is CodeJump;
-  void isMap(int i) => get(i) is Map<String, dynamic>;
-  void isList(int i) => get(i) is List;
+  bool isNum(int i) => get(i) is num;
+  bool isStr(int i) => get(i) is String;
+  bool isBool(int i) => get(i) is bool;
+  bool isJump(int i) => get(i) is CodeJump;
+  bool isMap(int i) => get(i) is Map<String, dynamic>;
+  bool isList(int i) => get(i) is List;
 
   dynamic get(int n) {
     if (stack.isEmpty) return null;
@@ -135,6 +170,54 @@ class CodeStack {
 
   List popMultiple(int amount) {
     return List.generate(amount, (i) => pop());
+  }
+
+  void eq() {
+    final a = pop();
+    final b = pop();
+    push(a == b);
+  }
+
+  void ne() {
+    final a = pop();
+    final b = pop();
+    push(a != b);
+  }
+
+  void less() {
+    final a = pop();
+    final b = pop();
+    push(a < b);
+  }
+
+  void lessEq() {
+    final a = pop();
+    final b = pop();
+    push(a <= b);
+  }
+
+  void greater() {
+    final a = pop();
+    final b = pop();
+    push(a > b);
+  }
+
+  void greaterEq() {
+    final a = pop();
+    final b = pop();
+    push(a >= b);
+  }
+
+  void and() {
+    final a = pop();
+    final b = pop();
+    push(a && b);
+  }
+
+  void or() {
+    final a = pop();
+    final b = pop();
+    push(a || b);
   }
 }
 
@@ -187,7 +270,171 @@ class CodeCellManager {
   // Returns which instruction to go to.
   int runInstruction(
       int i, CodeCellInstruction? instruction, CodeRuntimeContext context) {
-    // TODO: Write new instruction set
+    if (instruction == null) return i + 1;
+
+    if (instruction.name == "pop") {
+      context.stack.pop();
+    }
+
+    if (instruction.name == "link") {
+      context.stack.link(
+          int.parse(instruction.params[0]), int.parse(instruction.params[1]));
+    }
+
+    if (instruction.name == "pushNum") {
+      context.stack.pushNum(double.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "pushMap") {
+      context.stack.pushMap();
+    }
+
+    if (instruction.name == "pushEmptyList") {
+      context.stack.pushList();
+    }
+
+    if (instruction.name == "pushList") {
+      context.stack.pushList(int.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "pushFixedJump") {
+      context.stack.pushJump(int.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "pushReturnJump") {
+      context.stack.pushJump(i + 1);
+    }
+
+    if (instruction.name == "pushCurrentIP") {
+      context.stack.pushJump(i);
+    }
+
+    if (instruction.name == "pushString") {
+      context.stack.pushString(instruction.params.join(" "));
+    }
+
+    if (instruction.name == "add") {
+      context.stack.sum();
+    }
+
+    if (instruction.name == "subtract") {
+      context.stack.sub();
+    }
+
+    if (instruction.name == "multiply") {
+      context.stack.mult();
+    }
+
+    if (instruction.name == "divide") {
+      context.stack.div();
+    }
+
+    if (instruction.name == "exp") {
+      context.stack.upArrow();
+    }
+
+    if (instruction.name == "index") {
+      context.stack.index(
+          int.parse(instruction.params[0]), int.parse(instruction.params[1]));
+    }
+
+    if (instruction.name == "setIndex") {
+      context.stack.setIndex(int.parse(instruction.params[0]),
+          int.parse(instruction.params[1]), int.parse(instruction.params[2]));
+    }
+
+    if (instruction.name == "pushFrom") {
+      context.stack.pushFrom(int.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "branch") {
+      final jump1 = context.stack.get(-2) as CodeJump;
+      final jump2 = context.stack.get(-3) as CodeJump;
+      if (context.stack.get(-1) == true) {
+        return jump1.ip;
+      } else {
+        return jump2.ip;
+      }
+    }
+
+    if (instruction.name == "goto") {
+      return (context.stack.get(-1) as CodeJump).ip;
+    }
+
+    if (instruction.name == "stringify") {
+      context.stack.stringify(int.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "pushStringified") {
+      context.stack.pushStringified(int.parse(instruction.params[0]));
+    }
+
+    if (instruction.name == "pushSize") {
+      if (context.stack.isJump(-1)) {
+        context.stack.pushNum(8);
+      }
+      if (context.stack.isMap(-1)) {
+        context.stack.pushNum(
+          (context.stack.get(-1) as Map<String, dynamic>).length.toDouble(),
+        );
+      }
+      if (context.stack.isList(-1)) {
+        context.stack.pushNum(
+          (context.stack.get(-1) as List).length.toDouble(),
+        );
+      }
+      if (context.stack.isNum(-1)) {
+        context.stack.pushNum(8);
+      }
+      if (context.stack.isBool(-1)) {
+        context.stack.pushNum(1);
+      }
+      if (context.stack.isStr(-1)) {
+        context.stack.pushNum(
+          (context.stack.get(-1) as String).length.toDouble(),
+        );
+      }
+    }
+
+    if (instruction.name == "set") {
+      context.stack.set(
+        int.parse(instruction.params[0]),
+        int.parse(instruction.params[1]),
+      );
+    }
+
+    if (instruction.name == "eq") {
+      context.stack.eq();
+    }
+
+    if (instruction.name == "ne") {
+      context.stack.ne();
+    }
+
+    if (instruction.name == "less") {
+      context.stack.less();
+    }
+
+    if (instruction.name == "lessEq") {
+      context.stack.lessEq();
+    }
+
+    if (instruction.name == "greater") {
+      context.stack.greater();
+    }
+
+    if (instruction.name == "greaterEq") {
+      context.stack.greaterEq();
+    }
+
+    if (instruction.name == "and") {
+      context.stack.and();
+    }
+
+    if (instruction.name == "or") {
+      context.stack.or();
+    }
+
     return i + 1;
   }
 
