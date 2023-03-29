0be0b74b2e8c5cdc14306141498ad0a6a0c8b4a0
IonutParau <ionut.alex.parau@gmail.com>
2023-03-29 19:42:00 +0200

    Fixed a quantum biome bug

--- lib/logic/update/biomes.dart
+++ lib/logic/update/biomes.dart
@@ -78,7 +78,7 @@ void biome() {
           if (!cell.data.containsKey(p.key)) {
             cell.data[p.key] = p.def;
           } else {
-            if ((cell.data[p.key]?.runtimeType ?? p.def.runtimeType) != p.def.runtimeType) {
+            if (cell.data[p.key]?.runtimeType != p.def.runtimeType) {
               cell.data[p.key] = p.def;
             }
           }
