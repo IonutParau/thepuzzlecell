f1fba6fb4177b7eeeb421a1506e7f36eefa9cca2
uku1928 <86157399+uku1928@users.noreply.github.com>
2023-08-18 20:28:41 +0300

    fixed anvil

--- lib/logic/update/anvil.dart
+++ lib/logic/update/anvil.dart
@@ -9,22 +9,22 @@ void anvil() {
     final double lossUponLethalImpact = cell.data["impact_loss"].toDouble();
     final double terminalVelocity = cell.data["speed_limit"].toDouble();
 
-    cell.data["velocity"] += gravity;
     velocity += gravity;
     if(velocity > terminalVelocity) {
       velocity = terminalVelocity;
-      cell.data["velocity"] = terminalVelocity;
     }
+    cell.data["velocity"] = velocity;
 
-    if(doSpeedMover(x, y, dir, velocity.toInt(), velocity.toInt())) {
-      if(velocity >= breakingVelocity) {
-        final dx = frontX(x, dir);
-        final dy = frontY(y, dir);
-        if(grid.inside(dx, dy) && breakable(grid.at(dx, dy), dx, dy, dir, BreakType.gravity)) {
-          grid.set(dx, dy, Cell(dx, dy));
-        }
+    if(doSpeedMover(x, y, dir, velocity.toInt(), velocity.toInt())) return;
+    if(velocity >= breakingVelocity) {
+      final dx = frontX(x, dir);
+      final dy = frontY(y, dir);
+      if(grid.inside(dx, dy) && breakable(grid.at(dx, dy), dx, dy, dir, BreakType.gravity)) {
+        grid.set(dx, dy, Cell(dx, dy));
+        cell.data["velocity"] = velocity * (1 - lossUponLethalImpact);
+        return;
       }
-      cell.data["velocity"] = velocity * (1 - lossUponLethalImpact / 100);
     }
+    cell.data["velocity"] = 0;
   }, null, "anvil");
 }
